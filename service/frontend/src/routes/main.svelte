71e6c02555854cb72e6c4ccecd6a9926eb317a17
remiCzn <cazinremi2@gmail.com>
2023-01-03 16:06:16 +0100

    Add style for main page

--- service/frontend/src/routes/main.svelte
+++ service/frontend/src/routes/main.svelte
@@ -1,23 +1,16 @@
 <script>
     import axios from "axios";
-
+    import "./main.css"
     let data = "";
 
-    axios.get("http://localhost:3001").then((res) => {
-        data = JSON.stringify(res.data);
-    });
+    // axios.get("http://localhost:3001").then((res) => {
+    //     data = JSON.stringify(res.data);
+    // });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>This is data from backend:</p>
-<p>{data}</p>
-
-<style>
-    p {
-        color: darkred;
-    }
-    h1 {
-        font-style: italic;
-        color: darkblue;
-    }
-</style>
+<div class="main">
+    <h1 class="title">Hackathon</h1>
+    <p>{data}</p>
+    <input class="input text-lg p-0.5" type="date" id="start" value="2022-01-15">
+    <button class="input text-xl p-2">Plannifie ma journée</button>
+</div>
