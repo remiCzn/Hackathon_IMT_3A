77d8fd6d1ff8e0547f6ef6475c0583da28b3a6f9
remiCzn <cazinremi2@gmail.com>
2023-01-03 10:32:06 +0100

    Different port

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ const app = express();
 const directory = '/dist';
 app.use(express.static(__dirname + directory));
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, function () {
     console.log('Listening on', port);
 });
