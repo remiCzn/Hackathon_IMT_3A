7a0bb2b069f60ff11fb929ba1994e92e3a174502
gloxounet <mglock@outlook.fr>
2023-01-04 14:28:59 +0100

    custom tailwind

--- service/frontend/tailwind.config.cjs
+++ service/frontend/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				"soft-pink": "#EDB5A3",
+				"soft-purple": "#98A8EE",
+				"soft-yellow": "#FFF280",
+				"soft-blue": "#B8DFEF",
+				metal: "#4E4E4E",
+			},
+		},
+	},
+	plugins: [],
+};
