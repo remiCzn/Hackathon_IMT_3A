f60b946aa6d147225abc5ffb1eb21639230d8706
gloxounet <mglock@outlook.fr>
2023-01-04 09:06:32 +0100

    result item

--- service/frontend/src/components/ResultItem.svelte
+++ service/frontend/src/components/ResultItem.svelte
@@ -1,12 +1,26 @@
 <script>
-    import "./main.css"
+    export let data = {
+        activities:[],
+        date:""
+    }
 </script>
 
-<div class="main">
-    <h1 class="title">Vendredi 04 janvier 2023</h1>
-    <div>
-        <p>Balade à dos de poney</p>
-        <p>Repas: Macdo</p>
-        <p>Cinéma: La Reine des neiges</p>
+{#if data.activities.length === 0}
+    {#if data.date === ""}
+        <br>
+    {:else}
+        <h1 class="text-lg">Aucune activité n'est prévue pour le {data.date}</h1>
+    {/if}
+{:else}
+    <h1 class="text-lg font-semibold">{data.date}</h1>
+    <div class="gap-14 flex-column items-center text-center">
+    {#each data.activities as activity}
+        <div class="border-blue-200 border-2 bg-red-200 rounded-lg text-sm">
+            <h2 class="font-semibold">{activity.title}</h2>
+            <p class="">{activity.description}</p>
+            <p class="">{activity.time}</p>
+        </div>
+    {/each}
     </div>
-</div>
+{/if}
+
--- service/frontend/src/routes/+page.svelte
+++ service/frontend/src/routes/+page.svelte
@@ -1,18 +1,37 @@
 <script>
-    import axios from "axios";
-    let data = "";
+    let result = {
+        activities:[],
+        date:""
+    }
 
-    // axios.get("http://localhost:3001").then((res) => {
-    //     data = JSON.stringify(res.data);
-    // });
+    async function getActivities(date) {
+        const response = await fetch("http://localhost:3000/activities");
+        const data = await response.json();
+        return data;
+    }
+
+    function getActivitiesTemp(date){
+        result = {
+            activities:[
+                {title:"Déjeuner", description:"Déjeuner au restaurant", time:"12:00"},
+                {title:"Conférence", description:"Conférence sur le thème de la blockchain", time:"14:00"},
+            ],
+            date:"Jeudi 04 Février 2023"
+        }
+    }
+
     import "../app.css"
+    import ResultItem from "../components/ResultItem.svelte";
 </script>
 
 
 <h1 class="title">Hackathon</h1>
-<p>{data}</p>
 <input class="input text-lg p-0.5" type="date" id="start" value="2022-01-15">
-<button class="input text-xl p-2">Plannifie ma journée</button>
+<button on:click={getActivitiesTemp} class="input text-xl p-2">Plannifie ma journée</button>
+
+<br>
+
+<ResultItem data={result} />
 
 <style>
 
