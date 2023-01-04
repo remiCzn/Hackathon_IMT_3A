f8b2eac988a78ffbc2ae5dc71986083aaae6ed55
gloxounet <mglock@outlook.fr>
2023-01-04 09:16:06 +0100

    result +

--- service/frontend/src/components/ResultItem.svelte
+++ service/frontend/src/components/ResultItem.svelte
@@ -1,26 +1,13 @@
 <script>
-    export let data = {
-        activities:[],
-        date:""
+    export let activity = {
+        title:"",
+        description:"",
+        time:""
     }
 </script>
 
-{#if data.activities.length === 0}
-    {#if data.date === ""}
-        <br>
-    {:else}
-        <h1 class="text-lg">Aucune activité n'est prévue pour le {data.date}</h1>
-    {/if}
-{:else}
-    <h1 class="text-lg font-semibold">{data.date}</h1>
-    <div class="gap-14 flex-column items-center text-center">
-    {#each data.activities as activity}
-        <div class="border-blue-200 border-2 bg-red-200 rounded-lg text-sm">
-            <h2 class="font-semibold">{activity.title}</h2>
-            <p class="">{activity.description}</p>
-            <p class="">{activity.time}</p>
-        </div>
-    {/each}
-    </div>
-{/if}
-
+<div class="border-blue-200 border-2 bg-red-200 rounded-lg text-sm">
+    <h2 class="font-semibold">{activity.title}</h2>
+    <p class="">{activity.description}</p>
+    <p class="">{activity.time}</p>
+</div>
--- service/frontend/src/routes/+page.svelte
+++ service/frontend/src/routes/+page.svelte
@@ -4,34 +4,52 @@
         date:""
     }
 
-    async function getActivities(date) {
+    import { onMount } from 'svelte';
+	
+	let now = new Date(), month, day, year;
+	let dateString;
+	
+	onMount(()=> {
+        month = '' + (now.getMonth() + 1),
+        day = '' + now.getDate(),
+        year = now.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    dateString = [year, month, day].join('-');
+	})
+
+    async function getActivities() {
         const response = await fetch("http://localhost:3000/activities");
         const data = await response.json();
-        return data;
+        result = data;
     }
 
-    function getActivitiesTemp(date){
+    function getActivitiesTemp(){
         result = {
             activities:[
                 {title:"Déjeuner", description:"Déjeuner au restaurant", time:"12:00"},
                 {title:"Conférence", description:"Conférence sur le thème de la blockchain", time:"14:00"},
             ],
-            date:"Jeudi 04 Février 2023"
+            date:dateString
         }
     }
 
     import "../app.css"
-    import ResultItem from "../components/ResultItem.svelte";
+    import ResultsItem from "../components/ResultsItem.svelte";
 </script>
 
 
 <h1 class="title">Hackathon</h1>
-<input class="input text-lg p-0.5" type="date" id="start" value="2022-01-15">
+<input class="input text-lg p-0.5" type="date" id="start" bind:value={dateString}>
 <button on:click={getActivitiesTemp} class="input text-xl p-2">Plannifie ma journée</button>
 
 <br>
 
-<ResultItem data={result} />
+<ResultsItem data={result} />
 
 <style>
 
