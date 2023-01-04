a3ccacdaa05d77098e4b1d8629df1fd38f45a10a
gloxounet <mglock@outlook.fr>
2023-01-04 08:36:54 +0100

    structurals changes

--- service/frontend/src/routes/+page.svelte
+++ service/frontend/src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <script>
-    import Main from "./main.svelte";
-    import Result from "./result.svelte";
+    import axios from "axios";
+    let data = "";
+
+    // axios.get("http://localhost:3001").then((res) => {
+    //     data = JSON.stringify(res.data);
+    // });
     import "../app.css"
 </script>
 
-<!--<Main></Main>-->
-<Result></Result>
+
+<h1 class="title">Hackathon</h1>
+<p>{data}</p>
+<input class="input text-lg p-0.5" type="date" id="start" value="2022-01-15">
+<button class="input text-xl p-2">Plannifie ma journée</button>
+
+<style>
+
+</style>
