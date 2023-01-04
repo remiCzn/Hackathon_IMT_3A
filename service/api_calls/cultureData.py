3ab1568217158be64b6241a333533c533e4a2e88
Hugo <hugovedrine@hotmail.com>
2023-01-04 11:06:02 +0100

    Ajout commentaires

--- service/api_calls/cultureData.py
+++ service/api_calls/cultureData.py
@@ -6,24 +6,29 @@ import requests
 URL = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_equipements-publics-nantes-metropole&q=&rows=10000&facet=theme&facet=categorie&facet=type&facet=commune&refine.theme="
 CACHE = "data/culture.json"
 
+#Créer le fichier data pour stocker les données
 if not os.path.isdir("data"):
     os.mkdir('data')
-    
+
+#Requête les données de la DB de Nantes
 def getDataFromAPI(theme="Culture"):
     res = requests.get(URL+theme)
     if not res.ok:
         return res.status_code
     return res.json()
 
+#Ajoute à un élément son horaire d'ouverture et renvoie cet element modifié
 def addHoraire(elem):
     idJson = horaires.get_infos(elem["fields"]["nom_complet"], elem["fields"]["geo_shape"]["coordinates"])
     id = idJson["id"]
+    #Si l'api google ne retourne aucun résultat
     if id=="NA":
         elem["fields"]["horaire"] = "NA"
         elem["fields"]["adresse"] = "NA"
         return elem
     adresse = idJson["address"]
     horaire, weekday = horaires.get_horaires(id)
+    #Si on a pas d'horaire d'ouverture, on considère qu'il est ouvert
     if(horaire=="NA"):
         elem["fields"]["horaire"] = "11111111111111"
         return elem
@@ -35,15 +40,18 @@ def addHoraire(elem):
 def addHoraireData(data):
     return list(map(addHoraire, data))
 
+#Stocke l'ensemble des données de l'API de Nantes dans un Json
 def cacheData():
     dataCulture = getDataFromAPI()
     dataSport = getDataFromAPI("Sport et loisirs")
+    #On concatène les données ayant pour thème Culture et Sport et Loisir
     dataSport["records"] += dataCulture["records"]
     dataSport["records"] = addHoraireData(dataSport["records"])
     with open(CACHE, "w") as fichier:
         fichier.write(json.dumps(dataSport))
     return True
 
+#Retourne l'ensemble des données
 def getCachedData():
     with open(CACHE, 'r') as f:
         return json.load(f)["records"]
--- service/api_calls/restaurant.py
+++ service/api_calls/restaurant.py
@@ -8,22 +8,26 @@ CACHE = "data/restaurant.json"
 
 if not os.path.isdir("data"):
     os.mkdir('data')
-    
+
+#On récupère les données des restaurants depuis Nantes opendata
 def getDataFromAPI():
     res = requests.get(URL)
     if not res.ok:
         return res.status_code
     return res.content
 
+#Ajoute à un restaurant, son horaire d'ouverture et son adresse
 def addHoraire(elem):
     idJson = horaires.get_infos(elem["fields"]["nomoffre"], elem["fields"]["localisation"])
     id = idJson["id"]
+    #Si l'api de google n'a pas trouvé de résultat
     if(id=="NA"):
         elem["fields"]["horaire"] = "NA"
         elem["fields"]["adresse"] = "NA"
         return elem
     adresse = idJson["address"]
     horaire, weekday = horaires.get_horaires(id)
+    #Si il n'y a pas d'horaire d'ouverture, on considère que le lieu est ouvert
     if(horaire=="NA"):
         elem["fields"]["horaire"] = "11111111111111"
         return elem
@@ -36,6 +40,8 @@ def addHoraireData(data):
     data = json.loads(data)
     return list(map(addHoraire, data))
 
+#Supprime les addresses pour ne garder qu'un champ unique
+#L'adresse est déterminé via les coordonnées en utilisant l'api google
 def addresse(elem):
     if "adresse1" in elem["fields"]:
         elem["fields"].pop("adresse1")
@@ -46,6 +52,7 @@ def addresse(elem):
 
     return elem
 
+#Stocke les données des restaurants dans un json
 def cacheData():
     dataRestau = getDataFromAPI()
     dataRestau = addHoraireData(dataRestau)
@@ -55,9 +62,7 @@ def cacheData():
         fichier.write(json.dumps(dataRestau))
     return True
 
-if not os.path.isdir("data"):
-    os.mkdir('data')
-
+#Retourne l'ensemble des restaurants de Nantes
 def getCachedDataRestaurant():
     with open(CACHE, 'r') as f:
         data = json.load(f)
