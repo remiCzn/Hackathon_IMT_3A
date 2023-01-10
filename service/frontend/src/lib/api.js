8a2889f6168f895050ca7ec018201556e283b51f
remiCzn <cazinremi2@gmail.com>
2023-01-10 15:37:47 +0100

    adding lib to request backend from front

---
+++ service/frontend/src/lib/api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const client = axios.create({
+    baseURL: "http://api.chillpaper.fr"
+});
+
+export function dateToTimeId(day) {
+    let dayOfWeek = (((day.getDay() - 1) % 7) + 7) % 7;
+    let partOfDay = day.getHours() >= 12;
+    return 2 * dayOfWeek + partOfDay;
+}
+
+export function getActivity(day) {
+    return client.get("/activity", {
+        params: {
+            time: dateToTimeId(day)
+        }
+    }).then((res) => res.data);
+}
+
+export function getRestaurant(day) {
+    return client.get("/restaurant", {
+        params: {
+            time: dateToTimeId(day)
+        }
+    }).then((res) => res.data);
+}
+
+export function getAgenda(day) {
+    return client.get("/agenda", {
+        params: {
+            time: dateToTimeId(day)
+        }
+    }).then((res) => res.data);
+}
+
+(async () => {
+    const a = await getRestaurant(new Date());
+    console.log(a);
+    const b = await getActivity(new Date());
+    console.log(b);
+    const c = await getAgenda(new Date());
+    console.log(c);
+})();
