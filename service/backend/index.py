b5e892daab519ed2087aac588d60db20bc7e8524
Mashbrow <maxence.nesme@live.fr>
2023-01-10 17:27:10 +0100

    adding comments + finishing docs

--- service/backend/index.py
+++ service/backend/index.py
@@ -134,6 +134,19 @@ def selection_by_distance(mid_day, pos, r, data, hist):
 
 
 def selection_by_tags_distance(mid_day, pos, r, tags, data, hist):
+    """
+    Perform random selection of activity available at that time based on the distance
+    to the user and the tags provided.
+
+    :param mid_day: integer, an integer representing a half of a day, ranging from 0 to 13
+    :param pos: tuple(float, float), a tuple containing the latitude and the longitude of the user such that pos = (lat, long)
+    :param r: int, the max distance from the user to the point of interest in km
+    :param tags: list[string], the list of tags to which the user want the activity to belong to
+    :param data: list of dict, the data in which perform the random selection
+    :param hist: list of dict, the history of previously selected elements
+    :return: a dict corresponding to the activity chosen
+    
+    """
 
     assert type(mid_day) is int, "Received wrong type"
     assert type(tags) is list, "tags should be a list"
@@ -210,6 +223,18 @@ def by_distance_agenda(day, pos, r):
 
 
 def by_tags_distance_agenda(day, pos, r, tags):
+    """
+    Create an agenda respecting a distance criterion and tags criteria, consisting of randomly
+    selected activities and restaurant for the first half and second half of the day and 
+    for lunch and dinner.
+    :param day: int, ranging from 0 to 6, the day for which the function aims at creating an agenda
+    :param pos: tuple(float, float), a tuple containing the latitude and the longitude of the user such that pos = (lat, long)
+    :param r:  int, the max distance from the user to the point of interest in km
+    :param tags: list[string], the list of tags the user wants the activities to belong to
+    :return: list of dict, list of elements consisting of activities and restaurants, ordered by
+    appearance in the day.
+    
+    """
 
     assert type(day) is int, "Received wrong type"
 
@@ -260,6 +285,38 @@ def get_activity():
         return make_response(activity,200)
 
 
+@swag_from("./docs/swagger_activity_distance.yml")
+@app.route("/activity_distance", methods=['GET'])
+def get_activity_by_distance():
+    time = int(request.args.get('time'))
+    lat = float(request.args.get('lat'))
+    long = float(request.args.get('long'))
+    r = int(request.args.get('r'))
+    activity = selection_by_distance(time, (lat,long), r, data_activities, hist_activities)
+    if activity == "bof":
+        result = {"error": "no more activity not already seen"}
+        return make_response(result,400)
+    else:
+        return make_response(activity,200)
+
+
+@swag_from("./docs/swagger_activity_tags_distance.yml")
+@app.route("/activity_tags_distance", methods=['GET'])
+def get_activity_by_tags_distance():
+    time = int(request.args.get('time'))
+    lat = float(request.args.get('lat'))
+    long = float(request.args.get('long'))
+    r = int(request.args.get('r'))
+    tags = request.args.get('tags')
+    tags = tags.split(",")
+    activity = selection_by_tags_distance(time, (lat,long), r, tags, data_activities, hist_activities)
+    if activity == "bof":
+        result = {"error": "no more activity not already seen"}
+        return make_response(result,400)
+    else:
+        return make_response(activity,200)
+
+
 @swag_from("./docs/swagger_restaurant.yml")
 @app.route("/restaurant", methods=['GET'])
 def get_restaurant():
@@ -271,9 +328,39 @@ def get_restaurant():
     else:
         return make_response(restaurant, 200)
 
-###
-# TO UPDATE
-### 
+
+@swag_from("./docs/swagger_restaurant_distance.yml")
+@app.route("/restaurant_distance", methods=['GET'])
+def get_restaurant_by_distance():
+    time = int(request.args.get('time'))
+    lat = float(request.args.get('lat'))
+    long = float(request.args.get('long'))
+    r = int(request.args.get('r'))
+    activity = selection_by_distance(time, (lat,long), r, data_restaurants, hist_restaurants)
+    if activity == "bof":
+        result = {"error": "no more activity not already seen"}
+        return make_response(result,400)
+    else:
+        return make_response(activity,200)
+
+
+@swag_from("./docs/swagger_restaurant_tags_distance.yml")
+@app.route("/restaurant_tags_distance", methods=['GET'])
+def get_restaurant_by_tags_distance():
+    time = int(request.args.get('time'))
+    lat = float(request.args.get('lat'))
+    long = float(request.args.get('long'))
+    r = int(request.args.get('r'))
+    tags = request.args.get('tags')
+    tags = tags.split(",")
+    activity = selection_by_tags_distance(time, (lat,long), r, tags, data_restaurants, hist_restaurants)
+    if activity == "bof":
+        result = {"error": "no more activity not already seen"}
+        return make_response(result,400)
+    else:
+        return make_response(activity,200)
+
+
 @swag_from("./docs/swagger_agenda.yml")
 @app.route("/agenda", methods=['GET'])
 def get_agenda():
