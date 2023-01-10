61b51a61cace22d45d24a3129a178e2ddc1cf4e9
gloxounet <mglock@outlook.fr>
2023-01-10 12:04:48 +0100

    redirect

--- service/frontend/src/routes/plan/+page.svelte
+++ service/frontend/src/routes/plan/+page.svelte
@@ -62,7 +62,7 @@
 	/>
 
 	<Anchor
-		href="/"
+		href="/results"
 		class="bg-soft-pink px-2 py-4 rounded-3xl lg:w-10/12 xl:w-11/12"
 		bind:condition={validInformations}>Voir mon programme</Anchor
 	>
