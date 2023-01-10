a59c33de7675231ba08b17501ed41ef1f17516a4
remiCzn <cazinremi2@gmail.com>
2023-01-10 14:11:40 +0100

    deleting useless page

--- service/frontend/src/routes/planning/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Card from "../../components/Card/Card.svelte";
-</script>
-
-<div class="flex flex-col items-center">
-  <h2>Matinée</h2>
-  <Card />
-  <h2>Après-midi</h2>
-</div>
