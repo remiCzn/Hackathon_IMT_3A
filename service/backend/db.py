f83b193c09400b526ecbbc655018e9206890377b
remiCzn <cazinremi2@gmail.com>
2023-01-11 16:20:22 +0100

    Modify db hostname

--- service/backend/db.py
+++ service/backend/db.py
@@ -12,7 +12,7 @@ def db_connect():
         conn = mariadb.connect(
             user="herve",
             password="e92]sPW6FXhZ5y_)",
-            host="chillpaper.fr",
+            host="mariadb",
             port=3306,
             database="herve"
 
