ffa7e841bc8758bd2fd3e0950af56901a7f961ca
gloxounet <mglock@outlook.fr>
2023-01-17 14:11:58 +0100

    syntax error

--- service/frontend/src/routes/results/+page.server.js
+++ service/frontend/src/routes/results/+page.server.js
@@ -6,7 +6,7 @@ import {get} from "svelte/store"
 export async function load({url}) {
     const date = new Date(url.searchParams.get("date"));
     const lat = parseFloat(url.searchParams.get("latit"));
-    const long = parseFlaot(url.searchParams.get("long"));
+    const long = parseFloat(url.searchParams.get("long"));
     const range = parseInt(url.searchParams.get("rayon"));
     // Fetch data on chillpaper.fr/api
     const agenda = await getAgenda(date, get(historyStore), lat, long, range);
